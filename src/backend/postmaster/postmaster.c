19b1f01fe06c7962fe209e1451461d92dcb37be1
Don Seiler <don.seiler@breakthroughfuel.com>
2018-06-20 13:47:37 -0500

    Set port->application_name when processing startup packet

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2094,6 +2094,10 @@ retry1:
 											pstrdup(nameptr));
 				port->guc_options = lappend(port->guc_options,
 											pstrdup(valptr));
+
+				/* Copy application_name to port as well */
+				if (strcmp(nameptr, "application_name") == 0)
+					port->application_name = pstrdup(valptr);
 			}
 			offset = valoffset + strlen(valptr) + 1;
 		}
